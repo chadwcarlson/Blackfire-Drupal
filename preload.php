626c53844eb15657934271970bc5dfe24ab93838
chadcarlson <chad.carlson@platform.sh>
2021-09-29 14:55:45 -0400

    All in web save tests.'

--- preload.php
+++ preload.php
@@ -1,14 +1,9 @@
 <?php
-$directory = new RecursiveDirectoryIterator(getenv('PLATFORM_APP_DIR') . '/vendor');
-$iterator = new RecursiveIteratorIterator($directory);
-$regex = new RegexIterator($iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
+$directory = new RecursiveDirectoryIterator(getenv('PLATFORM_APP_DIR') . '/web');
+$fullTree = new RecursiveIteratorIterator($directory);
+$phpFiles = new RegexIterator($fullTree, '/.+((?<!Test)+\.php$)/i', RecursiveRegexIterator::GET_MATCH);
 
-foreach ($regex as $key => $file) {
-    // This is the important part!
-    // if ($file[0] != "app/vendor/doctrine/cache/lib/Doctrine/Common/Cache/RiakCache.php") {
-    //     opcache_compile_file($file[0]);
-    // }
-    // print_r($file[0]);
-    opcache_compile_file($file[0]);
-
+foreach ($phpFiles as $key => $file) {
+    require_once($file[0]);
 }
+?>
