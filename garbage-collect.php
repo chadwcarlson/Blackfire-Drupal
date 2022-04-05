7fa429aa86a5259f6a41222ea45ed979d9e577a1
chadcarlson <chad.carlson@platform.sh>
2022-04-05 12:09:26 -0400

    ini plus build.

--- garbage-collect.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Need active session to initialize session data storage access.
-session_start();
-
-// Executes GC immediately
-session_gc();
-
-// Clean up session ID created by session_gc()
-session_destroy();
